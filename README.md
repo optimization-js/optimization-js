e6133d305da3cdc6466dfa629ec8bd2d0333dcc3
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2015-09-14 22:20:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+A javascript library implementing useful multivariate function optimization procedures, which allow to find a local minimum of some function of a vector argument. Such argument is a javascript array. For example usage, see index.html.
+
+Currently, implemented are:
+
+**Modified version of the Powell zero order minimization method.** Very useful method for prototyping. Only requires that a function can be computed (derivative need not be provided!), and typically works fine for problems with 100 - 1000 variables (vector argument size). 
---
+++ js/optimization.js
@@ -0,0 +1,23 @@
+// optimjs stands for optimize javascript (duh :D)
+var optimjs = (function (exports) {
+
+    // export public members
+    exports = exports || {};
+
+    // ===================== data preprocessing ==========================
+    
+	exports.minimize = function(fnc, x0){
+		// fnc: function which takes array of size N as an input
+		// x0: array or real numbers of size N; 
+		// serves as initialization of algorithm.
+		
+		// solver precision
+		var eps = 1e-3;
+		
+		
+		
+	}
+	
+    return exports;
+
+})(typeof module != 'undefined' && module.exports);  // add exports to module.exports if in node.js
