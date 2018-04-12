c8941f33517eeb10f85e6031dc75bb66ce0f596a
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-04-12 10:49:11 +0200

    Initial test

---
+++ tests/optimization.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+var opt = require('../src/optimization')
+
+describe('Basic API', function(){
+    it('should work', function(){
+        expect('true').to.be.true;
+    })
+})
